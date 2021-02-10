f8b04b6538021c45b13174c72ade25084539d44c
teddy-beau <hello@teddy-beau.com>
2021-02-10 22:12:36 +0100

    header & hero updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,19 @@ import logo from "../assets/images/vinted-logo.png";
 
 const Header = () => {
    return (
-      <header className="container">
-         <Link to="/">
-            <img src={logo} alt="Logo Vinted" />
-         </Link>
-         <div className="search-bar">
-            <input type="search" />
-         </div>
-         <button>S'inscrire</button>
-         <button>Se connecter</button>
-         <div>Vends tes articles</div>
-      </header>
+      <div className="container">
+         <header>
+            <Link to="/">
+               <img src={logo} alt="Logo Vinted" />
+            </Link>
+            <div className="search-bar">
+               <input type="search" placeholder="Rechercher des articles" />
+            </div>
+            <button>S'inscrire</button>
+            <button>Se connecter</button>
+            <button>Vends tes articles</button>
+         </header>
+      </div>
    );
 };
 
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,15 @@
 import heroBanner from "../assets/images/hero-banner.jpeg";
+import tearHero from "../assets/images/tear-hero.svg";
 
 const Hero = () => {
    return (
       <div className="hero-banner">
-         <img src={heroBanner} alt="Banner" />
+         <img className="main-img" src={heroBanner} alt="Banner" />
+         <img
+            className="tear-img"
+            src={tearHero}
+            alt="Teared of paper effect"
+         />
          <div className="container">
             <div>
                <h1>Prêts à faire du tri dans vos placards ?</h1>
